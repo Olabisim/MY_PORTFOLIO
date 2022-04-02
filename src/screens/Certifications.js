3badce74fca426083a3d84a05019772e63e7de77
holabisii <ajoseholabisi@gmail.com>
2022-04-02 11:43:14 -0400

    certifications page done

--- src/screens/Certifications.js
+++ src/screens/Certifications.js
@@ -9,8 +9,28 @@ const Certifications = () => {
                 <div>
                         <Header />
 
-                        <div>
-                        
+                        <h2 className="heading-secondary center-head">
+                                CERTIFICATIONS
+                        </h2>
+                        <div className='certify_main'>
+                                <div className="certify_card">
+                                        <img src="../../cert2.PNG" alt="second_cert" />
+                                        <div className="certify_text">
+                                        <h1>The never ending journey</h1>
+                                        </div>
+                                </div>
+                                <div className="certify_card">
+                                        <img src="../../cert1.PNG" alt="first_cert" />
+                                        <div className="certify_text">
+                                                <h1>Adventure is always needed</h1>
+                                        </div>
+                                </div>
+                                <div className="certify_card">
+                                        <img src="../../cert3.PNG" alt="third_cert" />
+                                        <div className="certify_text">
+                                                <h1>Explore the vast beauty of nature</h1>
+                                        </div>
+                                </div>
                         </div>
 
                         <Footer />
