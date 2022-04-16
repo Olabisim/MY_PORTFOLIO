2f808d35b7461acea7d80bbea7ce06383bf30728
holabisii <ajoseholabisi@gmail.com>
2022-04-16 13:30:32 -0400

    mainHome component fixed

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -11,6 +11,7 @@ const Home = () => {
                 <div>
                         <Header />
                         <Navigation />
+                        <MainHome />
                         <Footer />
                 </div>
         )
