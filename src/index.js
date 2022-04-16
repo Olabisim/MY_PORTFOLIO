c1c1d4671598b5ba5a71cad025244f69fdb7a124
holabisii <ajoseholabisi@gmail.com>
2022-04-16 13:48:05 -0400

    removed warnings of import

--- src/index.js
+++ src/index.js
@@ -3,21 +3,8 @@ import {render} from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {
-      BrowserRouter as Router,
-      Routes,
-      Route
-} from 'react-router-dom';
-import Home from './screens/Home';
-import Certifications from './screens/Certifications';
 
 render(
-        // <Router>
-        //         <Routes>
-        //                 <Route path="/" elemenet={<Home />} exact />
-        //                 <Route path="/certification" elemenet={<Certifications />} />
-        //         </Routes>
-        // </Router>
         <App />
         ,
         document.getElementById('root')
