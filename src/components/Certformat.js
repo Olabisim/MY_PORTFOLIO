5ffe94317f56a354d7762c1c9ff4d3c639e5d87e
holabisii <ajoseholabisi@gmail.com>
2022-04-23 15:59:26 -0400

    medium screen size adjusted

---
+++ src/components/Certformat.js
@@ -0,0 +1,24 @@
+
+
+export const Certformat = () => {
+        return (
+                <div>
+                        <div className='certify_main'>
+                                <div className="certify_card">
+                                        <img src="../../cert2.jpg" alt="second_cert" />
+                                        <div className="certify_text">
+                                        <h1>DEVELOPER</h1>
+                                        </div>
+                                </div>
+                                <div className="certify_card">
+
+                                        <img src="../../cert1.jpg" alt="first_cert" />
+                                        <div className="certify_text">
+                                                <h1>DEVELOPER</h1>
+                                        </div>
+                                </div>
+                        
+                        </div>
+                </div>
+        )
+}
--- src/screens/Certifications.js
+++ src/screens/Certifications.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import '../styles/Card.css';
+import Navigation from '../components/Navigation';
 
 
 const Certifications = () => {
         return (
                 <div>
                         <Header />
+                        <Navigation />
 
                         <h2 className="heading-secondary center-head">
                                 CERTIFICATIONS
