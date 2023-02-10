1977bbf8bd19fc9c4bde0ac77a8b155d684ecea6
holabisii <ajoseholabisi@gmail.com>
2023-02-10 06:15:59 -0500

    changing the whole color

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,7 +4,7 @@ export const ProjectCard = ({title, children, link, linkName}) => {
         return (
                 
                 <div className="col-1-of-3 projectcard">
-                        <h2 className='heading-secondary'>{title}</h2>
+                        <h2 className='heading-secondary heading_title  '>{title}</h2>
                         <div className="card__details">
                         <ul>
                                 {children}
