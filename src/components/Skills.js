d477f0151a2efdfb48acc0f888c81372e47e38c3
olabisim <ajoseholabisi@gmail.com>
2024-01-17 16:51:12 -0800

    little vital changes

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,11 +7,11 @@ const Skills = ({name, skillsPage}) => {
   
                         <section className="skills-section my-skills" id="skills">
                         <h1 className="text-skills" data-aos="slide-up">{name}</h1>
-                        {
+                        {/* {
                                 skillsPage 
                                 &&
                                 <img src="https://raw.githubusercontent.com/Sacsam005/my_portfolio_website/0fc1dccb57e4a40e1751d695fbd5b8048fae6471/img/skills.svg" alt="Skills" data-aos="zoom-out" />
-                        }
+                        } */}
                         
                         </section>
                   
